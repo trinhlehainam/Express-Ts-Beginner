b2df51a6cad95f0bfc8dab45a7899fa4963f787e
checker <trinhlehainam@protonmail.com>
2021-11-16 13:53:43 +0900

    implement visible page for logged in user

--- src/controllers/homePage.ts
+++ src/controllers/homePage.ts
@@ -6,5 +6,6 @@ import '../utils/mysession'
 export async function homePageController(req: Request, res: Response) {
     const blogposts = await BlogPost.find({}); 
     console.log(req.session);
-    res.render('index', {blogposts});
+    const isLoggedIn = req.session.userId !== undefined;
+    res.render('index', {blogposts, isLoggedIn});
 }
--- src/utils/mysession.ts
+++ src/utils/mysession.ts
@@ -1,5 +1,6 @@
 import {SessionData} from 'express-session'
 
+// NOTE: use declaration merging
 declare module 'express-session' {
     interface SessionData {
         userId?: string
