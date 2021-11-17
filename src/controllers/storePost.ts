a07bfc202763a9b3f1b79b1d71f82d8ca26d2ddf
Trinh Le Hai Nam <trinhlehainam1607@gmail.com>
2021-11-17 10:04:47 +0900

    Fix directory when store image in new post

--- src/controllers/storePost.ts
+++ src/controllers/storePost.ts
@@ -8,10 +8,11 @@ import {BlogPost} from '../models/blogpost'
 //TODO: add check multiple files
 export async function storePostController(req: Request, res: Response, next: NextFunction) {
     const image = req.files ? req.files.image : undefined;
-    if(!image) return next();
-    if(!isSingleFile(image)) return next();
+    if(!image) return res.redirect('/');
+    if(!isSingleFile(image)) return res.redirect('/');
     try {
-        await image.mv(path.resolve(__dirname, 'public/img/', image.name));
+        //TODO: handle dirpath correctly
+        await image.mv(path.resolve('./public/img/', image.name));
         await BlogPost.create({
             ...req.body,
             imgPath: `/img/${image.name}`
